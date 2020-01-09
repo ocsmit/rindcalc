4c264b30bd3a812eb115c1156a1de326908abc92
ocsmit7654 <owen8461@protonmail.com>
2020-01-09 13:31:57 -0500

    Update

--- rindcalc/__init__.py
+++ rindcalc/__init__.py
@@ -3,9 +3,14 @@ import numpy as np
 from osgeo import gdal
 from glob import glob
 
+from .Get_Bands import GetBands
 from .AWEIsh_Index import AWEIsh_Index
 from .AWEIsh_Index_Masked import AWEIsh_Index_Masked
 from .MNDWI_Index import MNDWI_Index
 from .NDMI_Index import NDMI_Index
 from .NDVI_Index import NDVI_Index
 from .SAVI_Index import SAVI_Index
+from .NBLI_Index import NBLI_Index
+from .NDBaI_Index import NDBaI_Index
+from .NDBI_Index import NDBI_Index
+from .EBBI_Index import EBBI_Index
