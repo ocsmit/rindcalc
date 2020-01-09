b04c0859d9dc1ec068e424a3996edeff5c24106c
ocsmit7654 <owen8461@protonmail.com>
2020-01-09 13:25:34 -0500

    Create EBBI_Index.py

---
+++ rindcalc/EBBI_Index.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+from osgeo import gdal
+from glob import glob
+
+
+def EBBI_Index(landsat_dir, ebbi_out):
+    # Create list with file names
+    nir = glob(landsat_dir + "/*B5.tif")
+    swir1 = glob(landsat_dir + "/*B6.tif")
+    tir = glob(landsat_dir + "/*B10.tif")
+
+
+    # Open with gdal & create numpy arrays
+    gdal.UseExceptions()
+    gdal.AllRegister()
+    np.seterr(divide='ignore', invalid='ignore')
+    NIR_path = gdal.Open(os.path.join(landsat_dir, nir[0]))
+    nir_band = NIR_path.GetRasterBand(1).ReadAsArray().astype(np.float32)
+    SWIR1_path = gdal.Open(os.path.join(landsat_dir, swir1[0]))
+    swir1_band = SWIR1_path.GetRasterBand(1).ReadAsArray().astype(np.float32)
+    TIR_path = gdal.Open(os.path.join(landsat_dir, tir[0]))
+    tir_band = TIR_path.GetRasterBand(1).ReadAsArray().astype(np.float32)
+    snap = gdal.Open(os.path.join(landsat_dir, nir[0]))
+
+    # calculation
+    ebbi = ((swir1_band - nir_band) / (10 * (np.sqrt(swir1_band + tir_band))))
+
+    # Save Raster
+    if os.path.exists(ebbi_out):
+        raise IOError('EBBI raster already created')
+    if not os.path.exists(ebbi_out):
+        driver = gdal.GetDriverByName('GTiff')
+        metadata = driver.GetMetadata()
+        shape = ebbi.shape
+        dst_ds = driver.Create(ebbi_out, xsize=shape[1], ysize=shape[0], bands=1, eType=gdal.GDT_Float32)
+        proj = snap.GetProjection()
+        geo = snap.GetGeoTransform()
+        dst_ds.SetGeoTransform(geo)
+        dst_ds.SetProjection(proj)
+        dst_ds.GetRasterBand(1).WriteArray(ebbi)
+        dst_ds.FlushCache()
+        dst_ds = None
+
+    return print('EBBI index created.')
