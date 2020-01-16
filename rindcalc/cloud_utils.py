96809c55324e4cd36e430433a02dc1bbe745416e
Owen Smith <55674113+ocsmit@users.noreply.github.com>
2020-01-16 17:27:26 -0500

    wip cloud mask for index bands

--- rindcalc/cloud_utils.py
+++ rindcalc/cloud_utils.py
@@ -4,10 +4,11 @@ from osgeo import gdal
 from glob import glob
 
 
-def cloud_mask(landsat_dir, out):
+def cloud_mask(landsat_dir, band):
     # Get qa path
-    qa = glob(landsat_dri + "/*BQA.tif")
 
+    qa = glob(landsat_dri + "/*BQA.tif")
+    
     # Read band with gdal
     gdal.UseExceptions()
     gdal.AllRegister()
@@ -17,26 +18,29 @@ def cloud_mask(landsat_dir, out):
     qa_band = qa_path.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
     # Change cloud values to 0 everything else to 1
-    cloud_med = [2752, 2756, 2760, 2764, 3008, 3012, 3016, 3020, 
-				 3776, 3780, 3784, 3788, 6848, 6852, 6856, 6860,
-				 7104, 7108, 7112, 7116, 7876, 7880, 7884]
-	cloud_hi = [2800, 2804, 2808, 2812, 6896, 6900, 6904, 6908]
-	cloud = [2976, 2980, 2984, 2988, 3008, 3012, 3016, 3020, 
-				7072, 7076, 7080, 7084, 7104, 7108, 7112, 7116]				
+    mask_values = [2976, 2980, 2984, 2988, 3008, 3012, 3016, 3020, 
+	     	   7072, 7076, 7080, 7084, 7104, 7108, 7112, 7116]				
                 
     np.seterr(divide='ignore', invalid='ignore')
 	
-	m = np.ma.array(test, 
-		mask=np.logical_or.reduce([test == value for value in 
-		[2976, 2980, 2984, 2988, 3008, 3012, 3016, 3020, 2]]))
-	np.ma.set_fill_value(m, 0)
-	m1 = m.filled()
-	m1[m1 != 0] = 1
+    m = np.ma.array(qa_band, 
+    	mask=np.logical_or.reduce([test == value for value in 
+    	mask_values]))
+    np.ma.set_fill_value(m, 0)
+    m1 = m.filled()
+    m1[m1 != 0] = 1
 	
-	for i in range(len(cloud):
-		test1 = np.where((test == i), 0, 1))
-    raster[(raster)] = 0
-    raster_mask = np.ma.MaskedArray(raster, mask=(raster == np.median))
-    raster_mask.reshape(raster.shape)
+   m1.reshape(qa_band.shape)
+   
+   band * m1
+
+   return band 
+			
+			
+  
+
+   
+
+			
 	
     
