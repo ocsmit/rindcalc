971e5e739804bee44455371979aecef031a8d07c
Owen Smith <55674113+ocsmit@users.noreply.github.com>
2020-01-16 17:28:16 -0500

    add cloud mask

--- rindcalc/__init__.py
+++ rindcalc/__init__.py
@@ -2,3 +2,4 @@ from .index_utils import GetBands, AWEIsh, AWEInsh, NDMI, MNDWI, NDVI, \
     GNDVI, SAVI, ARVI, NDBI, NDBaI, NBLI, EBBI, UI, NBRI
 from .composite_utils import RGB, FalseColor
 from .class_utils import k_means
+from .class_utils import cloud_mask
