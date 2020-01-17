47d63e27ffa8d99596211059cad823aa0aa15329
ocsmit7654 <owen8461@protonmail.com>
2020-01-16 21:48:32 -0500

    Update tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,13 +2,13 @@ import rindcalc
 import time
 
 start_time = time.time()
-landsat_dir = 'C:/Imagery/Landsat_8/2019_11_28'
+landsat_dir = 'C:/Imagery/Landsat_8/aoi'
 landsat_dir1 = 'C:/Imagery/Landsat_8/2020_01_07_AUS'
 landsat_dir2 = 'C:/Imagery/Landsat_8/2018_08_19'
 input_raster = 'C:/Research/ALCC_RE/out/AWEIsh_unmasked.tif'
 out_raster = 'C:/tmp/NDVI.tif'
 
-rindcalc.NDVI(landsat_dir2, out_raster)
+rindcalc.RGB(landsat_dir, out_raster)
 end_time = time.time() - start_time
 print(end_time / 60)
 
