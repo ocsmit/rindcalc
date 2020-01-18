f27fead450f402bb44a83a317581049708b57648
Owen  Smith <55674113+owen8461@users.noreply.github.com>
2020-01-18 11:31:05 -0500

    Update

--- rindcalc/__init__.py
+++ rindcalc/__init__.py
@@ -1,5 +1,6 @@
-from .index_utils import GetBands, AWEIsh, AWEInsh, NDMI, MNDWI, NDVI, \
+from .index_utils import AWEIsh, AWEInsh, NDMI, MNDWI, NDVI, \
     GNDVI, SAVI, ARVI, NDBI, NDBaI, NBLI, EBBI, UI, NBRI
 from .composite_utils import RGB, FalseColor
 from .class_utils import k_means
 from .cloud_utils import cloud_mask
+from .bands_utils import GetBands, save_raster
