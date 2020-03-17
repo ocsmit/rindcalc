6542fa7d940deb84c641d3f4502a3b4d6708fd5e
ocsmit <owen8461@protonmail.com>
2020-03-17 17:16:11 -0400

    Theme color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,4 +54,7 @@ html_theme = 'sphinx_materialdesign_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_theme_options = {}
+html_theme_options = {
+    'primary_color': 'green'
+
+}
