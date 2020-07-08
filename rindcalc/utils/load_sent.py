30a8f01acd32197c1596b0bcfb9590df39df8df5
ocsmit <owen8461@protonmail.com>
2020-07-08 18:30:24 -0400

    Change band names

--- rindcalc/utils/load_sent.py
+++ rindcalc/utils/load_sent.py
@@ -21,28 +21,40 @@ def load_sent(path):
 
     one = gdal.Open(paths[0])
     band_1 = one.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     two = gdal.Open(paths[1])
     band_2 = two.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     three = gdal.Open(paths[2])
     band_3 = three.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     four = gdal.Open(paths[3])
     band_4 = four.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     five = gdal.Open(paths[4])
     band_5 = five.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     six = gdal.Open(paths[5])
     band_6 = six.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     seven = gdal.Open(paths[6])
     band_7 = seven.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     eight = gdal.Open(paths[7])
     band_8 = eight.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     eight_a = gdal.Open(paths[8])
     band_8a = eight_a.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     nine = gdal.Open(path[9])
     band_9 = nine.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     ten = gdal.Open(paths[10])
     band_10 = ten.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     eleven = gdal.Open(paths[11])
     band_11 = eleven.GetRasterBand(1).ReadAsArray().astype(np.float32)
+
     twelve = gdal.Open(paths[12])
     band_12 = twelve.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
