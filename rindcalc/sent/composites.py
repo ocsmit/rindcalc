01a9240c152c70de0b826564274889bca7e3c9d1
ocsmit <owen8461@protonmail.com>
2020-07-08 18:29:57 -0400

    Refactor sent composites to use load_sent

--- rindcalc/sent/composites.py
+++ rindcalc/sent/composites.py
@@ -5,73 +5,35 @@
 
 import numpy as np
 from osgeo import gdal
-from rindcalc.utils import norm
-from rindcalc.sent.sent_utils import get_bands
+from rindcalc.utils import norm, load_sent, save_comp
 
 
 def RGB(sent_dir, out_composite):
 
-    bands = get_bands(sent_dir)
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    gdal.UseExceptions()
+    gdal.AllRegister()
 
-    red = gdal.Open(bands[3])
-    red_band = norm(red.GetRasterBand(1).ReadAsArray().astype(np.float32),
-                    255, 0)
-    green = gdal.Open(bands[2])
-    green_band = norm(green.GetRasterBand(1).ReadAsArray().astype(np.float32),
-                      255, 0)
-    blue = gdal.Open(bands[1])
-    blue_band = norm(blue.GetRasterBand(1).ReadAsArray().astype(np.float32),
-                     255, 0)
-    snap = red
+    bands = load_sent(sent_dir)
 
-    driver = gdal.GetDriverByName('GTiff')
-    metadata = driver.GetMetadata()
-    shape = red_band.shape
-    dst_ds = driver.Create(out_composite,
-                           xsize=shape[1],
-                           ysize=shape[0],
-                           bands=3,
-                           eType=gdal.GDT_Byte)
-    proj = snap.GetProjection()
-    geo = snap.GetGeoTransform()
-    dst_ds.SetGeoTransform(geo)
-    dst_ds.SetProjection(proj)
-    dst_ds.GetRasterBand(1).WriteArray(red_band)
-    dst_ds.GetRasterBand(2).WriteArray(green_band)
-    dst_ds.GetRasterBand(3).WriteArray(blue_band)
-    dst_ds.FlushCache()
-    dst_ds = None
+    norm_red = norm(bands["band_4"], 255, 0)
+    norm_green = norm(bands["band_3"], 255, 0)
+    norm_blue = norm(bands["band_2"], 255, 0)
+
+    save_comp(norm_red, norm_green, norm_blue, bands["snap"])
 
 
 def bathymetry(sent_dir, out_composite):
 
-    bands = get_bands(sent_dir)
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    gdal.UseExceptions()
+    gdal.AllRegister()
 
-    red = gdal.Open(bands[3])
-    red_band = norm(red.GetRasterBand(1).ReadAsArray().astype(np.float32),
-                    255, 0)
-    green = gdal.Open(bands[2])
-    green_band = norm(green.GetRasterBand(1).ReadAsArray().astype(np.float32),
-                      255, 0)
-    coastal = gdal.Open(bands[0])
-    coastal_band = norm(coastal.GetRasterBand(1).ReadAsArray().astype(np.float32)
-                        , 255, 0)
-    snap = red
+    bands = load_sent(sent_dir)
 
-    driver = gdal.GetDriverByName('GTiff')
-    metadata = driver.GetMetadata()
-    shape = red_band.shape
-    dst_ds = driver.Create(out_composite,
-                           xsize=shape[1],
-                           ysize=shape[0],
-                           bands=3,
-                           eType=gdal.GDT_Byte)
-    proj = snap.GetProjection()
-    geo = snap.GetGeoTransform()
-    dst_ds.SetGeoTransform(geo)
-    dst_ds.SetProjection(proj)
-    dst_ds.GetRasterBand(1).WriteArray(red_band)
-    dst_ds.GetRasterBand(2).WriteArray(green_band)
-    dst_ds.GetRasterBand(3).WriteArray(coastal_band)
-    dst_ds.FlushCache()
-    dst_ds = None
+    norm_red = norm(bands["band_4"], 255, 0)
+    norm_green = norm(bands["band_3"], 255, 0)
+    norm_coastal = norm(bands["band_1"], 255, 0)
+
+    save_comp(norm_red, norm_green, norm_coastal, bands["snap"])
+
