23870eccc483ef7dd0d6e17fa3c669984e178125
ocsmit <owen8461@protonmail.com>
2020-07-14 00:44:21 -0400

    Rework the sent-2 load process

--- rindcalc/utils/load_sent.py
+++ rindcalc/utils/load_sent.py
@@ -5,60 +5,59 @@
 import os
 from osgeo import gdal
 import numpy as np
+from glob import glob
 
 
 def load_sent(path):
-    ends = ['B01.jp2', 'B02.jp2', 'B03.jp2', 'B04.jp2', 'B05.jp2',
-            'B06.jp2', 'B07.jp2', 'B08.jp2', 'B8A.jp2', 'B09.jp2',
-            'B10.jp2', 'B11.jp2']
+    ends = ['*B01.jp2', '*B02.jp2', '*B03.jp2', '*B04.jp2', '*B05.jp2',
+            '*B06.jp2', '*B07.jp2', '*B08.jp2', '*B8A.jp2', '*B09.jp2',
+            '*B10.jp2', '*B11.jp2', '*B12.jp2']
 
     paths = []
     for i in range(len(ends)):
-        for dir, sub, files in os.walk(path):
-            for f in files:
-                if f.endswith(ends[i]):
-                    paths.append(os.path.join(dir, f))
+        paths.append(glob(os.path.join(path, ends[i])))
+        #print(paths[i])
 
-    one = gdal.Open(paths[0])
+    one = gdal.Open(paths[0][0])
     band_1 = one.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    two = gdal.Open(paths[1])
+    two = gdal.Open(paths[1][0])
     band_2 = two.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    three = gdal.Open(paths[2])
+    three = gdal.Open(paths[2][0])
     band_3 = three.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    four = gdal.Open(paths[3])
+    four = gdal.Open(paths[3][0])
     band_4 = four.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    five = gdal.Open(paths[4])
+    five = gdal.Open(paths[4][0])
     band_5 = five.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    six = gdal.Open(paths[5])
+    six = gdal.Open(paths[5][0])
     band_6 = six.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    seven = gdal.Open(paths[6])
+    seven = gdal.Open(paths[6][0])
     band_7 = seven.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    eight = gdal.Open(paths[7])
+    eight = gdal.Open(paths[7][0])
     band_8 = eight.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    eight_a = gdal.Open(paths[8])
+    eight_a = gdal.Open(paths[8][0])
     band_8a = eight_a.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    nine = gdal.Open(path[9])
+    nine = gdal.Open(paths[9][0])
     band_9 = nine.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    ten = gdal.Open(paths[10])
+    ten = gdal.Open(paths[10][0])
     band_10 = ten.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    eleven = gdal.Open(paths[11])
+    eleven = gdal.Open(paths[11][0])
     band_11 = eleven.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    twelve = gdal.Open(paths[12])
+    twelve = gdal.Open(paths[12][0])
     band_12 = twelve.GetRasterBand(1).ReadAsArray().astype(np.float32)
 
-    snap = gdal.Open(paths[1])
+    snap = gdal.Open(paths[1][0])
 
     bands = {"band_1": band_1, "band_2": band_2, "band_3": band_3,
              "band_4": band_4, "band_5": band_5, "band_6": band_6,
