1589025c294bcc4d218bb0fbaf34993cc7e59ef9
ocsmit <owen8461@protonmail.com>
2020-07-13 22:59:35 -0400

    Add all bands into load_ls

--- rindcalc/utils/load_ls.py
+++ rindcalc/utils/load_ls.py
@@ -12,7 +12,9 @@ def load_ls(landsat_dir, dType=np.float32):
     nir = glob(os.path.join(landsat_dir, '*B5*'))
     swir1 = glob(os.path.join(landsat_dir, '*B6*'))
     swir2 = glob(os.path.join(landsat_dir, '*B7*'))
-    tir = glob(os.path.join(landsat_dir, '*B10*'))
+    pan = glob(os.path.join(landsat_dir, '*B8*'))
+    tir1 = glob(os.path.join(landsat_dir, '*B9*'))
+    tir2 = glob(os.path.join(landsat_dir, '*B10*'))
 
     coastal_path = gdal.Open(os.path.join(landsat_dir, ca[0]))
     band_1 = coastal_path.GetRasterBand(1).ReadAsArray().astype(dType)
@@ -35,13 +37,20 @@ def load_ls(landsat_dir, dType=np.float32):
     SWIR2_path = gdal.Open(os.path.join(landsat_dir, swir2[0]))
     band_7 = SWIR2_path.GetRasterBand(1).ReadAsArray().astype(dType)
 
-    tir_path = gdal.Open(os.path.join(landsat_dir, tir[0]))
+    pan_path = gdal.Open(os.path.join(landsat_dir, pan[0]))
+    band_8 = pan_path.GetRasterBand(1).ReadAsArray().astype(dType)
+
+    tir1_path = gdal.Open(os.path.join(landsat_dir, tir1[0]))
+    band_9 = tir1_path.GetRasterBand(1).ReadAsArray().astype(dType)
+
+    tir_path = gdal.Open(os.path.join(landsat_dir, tir2[0]))
     band_10 = tir_path.GetRasterBand(1).ReadAsArray().astype(dType)
 
     snap = gdal.Open(os.path.join(landsat_dir, blue[0]))
 
-    band_dict = {"band_1": band_1, "band_2": band_2, "band_3": band_3, "band_4": band_4,
-                 "band_5": band_5, "band_6": band_6, "band_7": band_7,
+    band_dict = {"band_1": band_1, "band_2": band_2, "band_3": band_3,
+                 "band_4": band_4, "band_5": band_5, "band_6": band_6,
+                 "band_7": band_7, "band_8": band_8,  "band_9": band_9,
                  "band_10": band_10, "snap": snap}
 
     return band_dict
