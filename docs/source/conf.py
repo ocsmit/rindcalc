1dad0b55cbc4c2139e4162ccbce7dc7bf504c7ae
ocsmit <owen8461@protonmail.com>
2020-07-27 01:10:00 -0400

    html for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+import sphinx_rtd_theme
 
 
 autodoc_mock_imports = ["numpy", "osgeo"]
@@ -38,7 +39,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.coverage",
-    'sphinx.ext.napoleon']
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -61,12 +63,11 @@ autodoc_default_options = {
 # a list of builtin themes.
 
 html_logo = 'rindcalc_logo.png'
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {
-    "github_url": "https://github.com/ocsmit/rindcalc"
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
+
